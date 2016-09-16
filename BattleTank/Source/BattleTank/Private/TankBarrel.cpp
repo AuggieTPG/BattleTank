99d402a0cff7c48907aa7c61b88087fca753bdb7
Quentin <qtimmons@shaw.ca>
2016-09-15 23:05:33 -0700

    hopefully last version of tank  barrel pitch

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,11 +7,12 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// move the barrel the right amount this frame
 	//given a max elevation speed and the frame time
-	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
 
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
-	auto RawElevationChange = RelativeRotation.Pitch + ElevationChange;
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
 
+	
+	auto RawElevationChange = RelativeRotation.Pitch + ElevationChange;
 	RawElevationChange = FMath::Clamp<float>(RawElevationChange, MinElevationDegrees, MaxElevationDegrees);
 
 	SetRelativeRotation(FRotator(RawElevationChange, 0, 0));
